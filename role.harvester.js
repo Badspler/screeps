12a687126bdda4617dc587d0f710c728d77e2cba
chris <letter103@hotmail.com>
2017-11-26 11:07:36 +1300

    bug fix: Harvester not changing out if mineing to transfering

--- role.harvester.js
+++ role.harvester.js
@@ -52,7 +52,7 @@ module.exports = {
 
 
         //No Energy left so will go get some
-        if(creep.carry.energy < creep.carryCapacity || creep.memory.action == 'mineing') {
+        if(creep.carry.energy < creep.carryCapacity && creep.memory.action == 'mineing') {
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
